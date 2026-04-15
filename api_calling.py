28b5a57443bffdaa49b9de3e5df6d5ee96391f29
Rohit Roy <roy172758@gmail.com>
2026-04-15 08:46:11 +0600

    Add files via upload

--- api_calling.py
+++ api_calling.py
@@ -2,7 +2,7 @@ from google import genai
 from dotenv import load_dotenv
 import os ,io
 from gtts import gTTS
-
+from google.genai import errors
 
 
 
@@ -20,21 +20,27 @@ client = genai.Client(api_key= my_api_key)
 # note generator 
 def note_generator(images):
 
-    prompt = """Summarize the picture in note format in language Bangla at max 100 words
+    prompt = """Summarize the picture in note format at max 100 words
     make sure to add necessary markdown to differentiate different section"""
 
 
-    response = client.models.generate_content(
-        model = "gemini-3-flash-preview",
-        contents=[images,prompt]
-    )
-
-    return response.text 
+    try:
+        response = client.models.generate_content(
+            model="gemini-3-flash",  # Using stable model name
+            contents=[images, prompt]
+        )
+        return response.text 
+    except errors.ClientError as e:
+        
+        return "🛑 Quota Exceeded: Daily limit reached (20 free requests). Try again later or upgrade plan."
+        
+    except Exception as e:
+        return f"⚠️ Error: {str(e)}" 
 
 
 
 def audio_transcription(text):
-    speech = gTTS(text,lang='bn',slow=False)
+    speech = gTTS(text,lang='en',slow=False)
     audio_buffer = io.BytesIO()
     speech.write_to_fp(audio_buffer)
     return audio_buffer
@@ -45,9 +51,15 @@ def quiz_generator(image,difficulty):
     prompt = f"Generate 3 quizzes based on the {difficulty}. Make sure to add markdown to differentiate the options. Add correct answer too,after the quiz"
 
 
-    response = client.models.generate_content(
-        model = "gemini-3-flash-preview",
-        contents=[image,prompt]
-    )
-
-    return response.text 
+    try:
+        response = client.models.generate_content(
+            model="gemini-3-flash",
+            contents=[image, prompt]
+        )
+        return response.text
+    except errors.ClientError as e:
+        
+        return "🛑 Quota Exceeded: Daily limit reached (20 free requests). Try again later or upgrade plan."
+        
+    except Exception as e:
+        return f"⚠️ Error: {str(e)}" 
